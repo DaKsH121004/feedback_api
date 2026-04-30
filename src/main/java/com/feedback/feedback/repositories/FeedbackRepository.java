a88020ce0626dd25a7e919bf7f4b406936bcd59b
Daksh121004 <dakshpandit8076@gmail.com>
2026-04-30 14:02:34 +0530

    fix: missing List import in FeedbackRepository

--- src/main/java/com/feedback/feedback/repositories/FeedbackRepository.java
+++ src/main/java/com/feedback/feedback/repositories/FeedbackRepository.java
@@ -4,6 +4,8 @@ import com.feedback.feedback.entities.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
     Boolean existsByStudentRollNoAndFacultyIdAndCourseId(String   studentRollNo, Long  facultyId, Long  courseId);
     @Query("SELECT AVG(f.averageRating) FROM Faculty f")
